880c2a382e0579534ed8874e5f961ad5f12e9b99
Amine Tahiri <atahiri@student.1337.ma>
2021-11-24 20:03:30 +0100

    creating the structs

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: atahiri <atahiri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 11:39:28 by atahiri           #+#    #+#             */
-/*   Updated: 2021/11/24 13:10:01 by atahiri          ###   ########.fr       */
+/*   Updated: 2021/11/24 19:26:54 by atahiri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	error_handling(int argc, char **args)
 	int j;
 
 	i = 0;
-	if (argc != 6)
+	if (argc != 5 && argc != 6)
 		return (1);
 	while (args[++i])
 	{
@@ -36,8 +36,5 @@ int		main(int argc, char **argv)
 {
 	if (error_handling(argc, argv))
 		printf("Error in arguments\n");
-	struct timeval now;
-	gettimeofday(&now, NULL);
-	printf("%ld", (now.tv_sec * 1000) - (now.tv_usec / 1000));
 	return (0);
 }
--- philosophers.h
+++ philosophers.h
@@ -6,7 +6,7 @@
 /*   By: atahiri <atahiri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 11:40:00 by atahiri           #+#    #+#             */
-/*   Updated: 2021/11/24 12:53:33 by atahiri          ###   ########.fr       */
+/*   Updated: 2021/11/24 19:39:31 by atahiri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,32 @@
 #include <pthread.h>
 #include <sys/time.h>
 
-typedef struct	s_philo
-{
-	int		philos_nb;
-	int		time_to_die;
-	int		time_to_eat;
-	int		time_to_sleep;
-	int		must_eat_nb;
-}				t_philo;
+typedef enum s_status {
+	EATING,
+	SLEEPING,
+	THINKING,
+	DEAD
+}			t_status;
 
+typedef struct	s_global {
+	int					philos_nb;
+	int					time_to_die;
+	int					time_to_eat;
+	int					time_to_sleep;
+	int					must_eat_nb;
+	pthread_mutex_t		*forks;
+	pthread_mutex_t		printing_lock;
+}				t_global;
+
+typedef struct s_philo {
+	t_status			status;
+	int					id;
+	int					nb_eats;
+	int					last_eat;
+	pthread_mutex_t		eat_lock;
+	pthread_t			t_id;
+	t_global			*state;
+}				t_philo;
 
 int		ft_atoi(const char *str);
 int		ft_isalpha(int c);
