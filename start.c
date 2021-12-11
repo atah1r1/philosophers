176c78d9779d17fda8f2d109c1d0886b149a8c72
Amine Tahiri <atahiri@student.1337.ma>
2021-12-11 11:56:16 +0100

    i think i finished

--- start.c
+++ start.c
@@ -6,15 +6,15 @@
 /*   By: atahiri <atahiri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 13:27:24 by atahiri           #+#    #+#             */
-/*   Updated: 2021/12/11 10:41:52 by atahiri          ###   ########.fr       */
+/*   Updated: 2021/12/11 11:54:48 by atahiri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-void	*routine(void *data)
+void *routine(void *data)
 {
-	t_philo	*philo;
+	t_philo *philo;
 
 	philo = (t_philo *)data;
 	while (1)
@@ -28,21 +28,25 @@ void	*routine(void *data)
 	return NULL;
 }
 
-int		check_number_of_eatings(t_philo philo, int nb)
+int check_number_of_eatings(t_philo *philos)
 {
-	if (nb == philo.state->must_eat_nb)
+	int i = -1;
+
+	while (++i < philos->state->philos_nb)
 	{
-		pthread_mutex_lock(&philo.state->printing_lock);
-		printf("each philosopher ate %d times", philo.state->must_eat_nb);
-		return (1);
+		if (philos->state->must_eat_nb > philos[i].nb_eats)
+		{
+			return (0);
+		}
+			// printf("MUST EAT NB === %d\n", philos[i].nb_eats);
 	}
-	return (0);
+	return (1);
 }
 
-void	*supervisor(void *philos)
+void *supervisor(void *philos)
 {
-	t_philo			*philo;
-	int				i;
+	t_philo *philo;
+	int i;
 
 	philo = (t_philo *)philos;
 	// printf ("nb %d\n", philo[0].state->philos_nb);
@@ -55,28 +59,30 @@ void	*supervisor(void *philos)
 			// printf ("id %d \ntime : %lld\n,  last: %lld\n , start: %lld\n, status %d\n",philo[i].id,
 			// (get_time() - philo[i].last_eat), philo[i].last_eat, get_time(), philo[i].status);
 
-			if ((get_time() - philo[i].last_eat) > philo[i].state->time_to_die
-				&& !philo[i].eat)
+			if ((get_time() - philo[i].last_eat) > philo[i].state->time_to_die && !philo[i].eat)
 			{
 				pthread_mutex_lock(&philo->state->printing_lock);
 				pthread_mutex_lock(&philo[i].eating);
 				printf("%lld %d dead\n", timestamp(philo), philo->id + 1);
 				return (NULL);
 			}
-			usleep(100);
-			if (philo->state->it_must_eat == 1 && check_number_of_eatings(philo[i], philo[i].nb_eats))
+			if (philo->state->it_must_eat == 1 && check_number_of_eatings(philos))
+			{
+				// pthread_mutex_lock(&philo->state->printing_lock);
+				// printf("all the philosopher has eaten at least %d times each.", philo->state->must_eat_nb);
 				return (NULL);
+			}
 			i++;
 		}
 	}
 	return (NULL);
 }
 
-int		start(pthread_t	*threads, t_philo *philos)
+int start(pthread_t *threads, t_philo *philos)
 {
 	puts("start");
-	int			i;
-	pthread_t	super;
+	int i;
+	pthread_t super;
 
 	i = -1;
 	while (++i < philos->state->philos_nb)
@@ -87,8 +93,5 @@ int		start(pthread_t	*threads, t_philo *philos)
 	}
 	pthread_create(&super, NULL, supervisor, (void *)philos);
 	pthread_join(super, NULL);
-	i = -1;
-	while (++i < philos->state->philos_nb)
-		pthread_mutex_destroy(&philos[0].state->forks[i]);
 	return (1);
-}	
+}
